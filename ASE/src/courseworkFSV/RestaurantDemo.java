766581ddee62479f38bb6f8ca106e15e7878a644
noelstepha <noelstepha@eisti.eu>
2014-02-06 19:48:01 +0100

    Add eclipse project with basic classes

---
+++ ASE/src/courseworkFSV/RestaurantDemo.java
@@ -0,0 +1,10 @@
+package courseworkFSV;
+
+public class RestaurantDemo {
+
+	
+	public static void main(String[] args){
+		Restaurant restaurant = new Restaurant();
+		System.out.println("Started2.");
+	}
+}
---
+++ ASE/src/courseworkFSV/RestaurantInterface.java
@@ -0,0 +1,33 @@
+package courseworkFSV;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+public class RestaurantInterface extends JFrame implements ActionListener {
+
+	
+	private Restaurant restaurant;
+	
+	public RestaurantInterface(Restaurant r){
+		super("Bill Information for table");
+		restaurant = r;		
+	}
+	
+	public void run(){
+	
+		
+	
+		
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
