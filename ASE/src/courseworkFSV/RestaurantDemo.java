92a5437a65439e51a907680e2736b47ea0b62896
florentgonin <florentgonin@aol.fr>
2014-02-19 18:01:59 +0000

    demo - export

--- ASE/src/courseworkFSV/RestaurantDemo.java
+++ ASE/src/courseworkFSV/RestaurantDemo.java
@@ -5,7 +5,10 @@ public class RestaurantDemo {
 	
 	public static void main(String[] args){
 		Restaurant restaurant = new Restaurant("menu.txt","orders.txt");
-		
+
+		//export test
+		restaurant.export("testExport.txt");
+
 		System.out.println("Finish.");
 		
 		RestaurantInterface c = new RestaurantInterface(restaurant);
