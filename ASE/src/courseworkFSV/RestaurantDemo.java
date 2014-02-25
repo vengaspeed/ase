9f3b1a7b452c9cf7625e6a8a6f004f842814b3e3
vengaspeed <vengaspeed@gmail.com>
2014-02-25 18:30:51 +0000

    report printed by interface

--- ASE/src/courseworkFSV/RestaurantDemo.java
+++ ASE/src/courseworkFSV/RestaurantDemo.java
@@ -7,7 +7,7 @@ public class RestaurantDemo {
 		Restaurant restaurant = new Restaurant("menu.txt","orders.txt");
 
 		//export test
-		restaurant.export("testExport.txt");
+		//restaurant.export("testExport.txt"); is done by the Do the report summary button on the first JFrame of interface
 
 		System.out.println("Finish.");
 		
